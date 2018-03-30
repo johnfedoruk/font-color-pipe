44bdf68689729420f2a6a425553720610ddca52c
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-30 13:45:22 -0500

    Added undefined tests for FontColorPipe

--- src/app/modules/font-color/pipes/font-color-pipe.pipe.spec.ts
+++ src/app/modules/font-color/pipes/font-color-pipe.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { FontColorPipePipe } from './font-color-pipe.pipe';
 
+let pipe: FontColorPipePipe;
+
 describe('FontColorPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FontColorPipePipe();
-    expect(pipe).toBeTruthy();
-  });
+    beforeEach(()=>{
+        pipe = new FontColorPipePipe();
+    })
+    describe('ContrastPipe', () => {
+        it('create an instance', () => {
+            expect(pipe).toBeTruthy();
+        });
+        it('should return #ffffff if passed undefined');
+        it('should return #ffffff if passed null');
+        it('should return #ffffff if passed 1');
+        it('should return #ffffff if passed ""');
+        it('should return #ffffff if passed #fffff');
+        it('should return #ffffff if passed #000000');
+        it('should return #000000 if passed #ffffff');
+        it('should return #ffffff if passed wiser #7b69df');
+        it('should return #ffffff if passed wiser #00ff00');
+    });
+
 });
