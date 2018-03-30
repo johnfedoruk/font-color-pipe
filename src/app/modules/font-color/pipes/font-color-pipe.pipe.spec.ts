48f140288092a792347b85e1989f143a72eb5cc0
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-30 14:02:51 -0500

    Implemented tests for FontColorPipe

--- src/app/modules/font-color/pipes/font-color-pipe.pipe.spec.ts
+++ src/app/modules/font-color/pipes/font-color-pipe.pipe.spec.ts
@@ -3,22 +3,40 @@ import { FontColorPipePipe } from './font-color-pipe.pipe';
 let pipe: FontColorPipePipe;
 
 describe('FontColorPipePipe', () => {
-    beforeEach(()=>{
+    beforeEach(() => {
         pipe = new FontColorPipePipe();
     })
-    describe('ContrastPipe', () => {
+    describe('FontColorPipe', () => {
         it('create an instance', () => {
             expect(pipe).toBeTruthy();
         });
-        it('should return #ffffff if passed undefined');
-        it('should return #ffffff if passed null');
-        it('should return #ffffff if passed 1');
-        it('should return #ffffff if passed ""');
-        it('should return #ffffff if passed #fffff');
-        it('should return #ffffff if passed #000000');
-        it('should return #000000 if passed #ffffff');
-        it('should return #ffffff if passed wiser #7b69df');
-        it('should return #ffffff if passed wiser #00ff00');
+        it('should return #FFFFFF if passed undefined', () => {
+            expect(pipe.transform(undefined)).toBe("#FFFFFF");
+        });
+        it('should return #FFFFFF if passed null',()=>{
+            expect(pipe.transform(null)).toBe("#FFFFFF");
+        });
+        it('should return #FFFFFF if passed 1',()=>{
+            expect(pipe.transform(1)).toBe("#FFFFFF");
+        });
+        it('should return #FFFFFF if passed ""',()=>{
+            expect(pipe.transform("")).toBe("#FFFFFF");
+        });
+        it('should return #FFFFFF if passed #fffff',()=>{
+            expect(pipe.transform("")).toBe("#FFFFFF");
+        });
+        it('should return #FFFFFF if passed #000000',()=>{
+            expect(pipe.transform("")).toBe("#FFFFFF");
+        });
+        it('should return #000000 if passed #ffffff',()=>{
+            expect(pipe.transform("#ffffff")).toBe("#000000");
+        });
+        it('should return #FFFFFF if passed #7b69df',()=>{
+            expect(pipe.transform("#7b69df")).toBe("#FFFFFF");
+        });
+        it('should return #FFFFFF if passed #00ff00',()=>{
+            expect(pipe.transform("#00ff00")).toBe("#FFFFFF");
+        });
     });
 
 });
