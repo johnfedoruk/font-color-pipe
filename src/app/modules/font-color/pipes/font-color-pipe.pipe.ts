4b7c8ef35df42db0170822aa122c52f27b3483ba
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-30 13:46:50 -0500

    Added FontColorPipe definition

--- src/app/modules/font-color/pipes/font-color-pipe.pipe.ts
+++ src/app/modules/font-color/pipes/font-color-pipe.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FontColorPipePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return "white";
-  }
+    transform(color_hex: any): any {
+        if (color_hex === undefined || color_hex === null || color_hex.length < 7) {
+            return '#FFFFFF';
+        }
+        const R_HEX: string = color_hex.substr(1, 2);
+        const G_HEX: string = color_hex.substr(3, 2);
+        const B_HEX: string = color_hex.substr(5, 2);
+        const R_DEC: number = parseInt(R_HEX, 16);
+        const G_DEC: number = parseInt(G_HEX, 16);
+        const B_DEC: number = parseInt(B_HEX, 16);
+        const CONTRAST_HEX: string = R_DEC * 0.299 + G_DEC * 0.587 + B_DEC * 0.114 > 186 ? '#000000' : '#FFFFFF';
+        return CONTRAST_HEX;
+    }
 
 }
