0bc1c97a8564f6b32753127fea1f65a2b84620d4
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-30 21:22:46 -0500

    Linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { environment } from '../environments/environment';
-import { FontColorPipe } from "./modules/font-color/pipes/font-color.pipe";
+import { FontColorPipe } from './modules/font-color/pipes/font-color.pipe';
 
 const MIN = 0;
 const MAX = 16777215;
@@ -14,7 +14,7 @@ const MS_IN_S = 333;
 })
 export class AppComponent implements OnInit {
     private _bg: string;
-    constructor(private ref: ElementRef) {}
+    constructor(private ref: ElementRef) { }
     public ngOnInit(): void {
         this.bg = '#FFFFFF';
         setInterval(() => this.bg = this.randomColor(), 3 * MS_IN_S);
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
         do {
             const DECIMAL: number = Math.floor(Math.random() * MAX) + MIN;
             hex = `#${DECIMAL.toString(HEX_BASE)}`;
-        } while(fontColorPipe.transform(hex)===PREV_HEX);
+        } while (fontColorPipe.transform(hex) === PREV_HEX);
         return hex;
     }
     public onOpenGithub(): void {
